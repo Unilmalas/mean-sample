05fdd1f1b463b81b563e88ee522cd9b22d496173
Bernd Schossmann <bschoss@yahoo.com>
2016-10-20 06:02:33 +0200

    file upload working
    still need to process file data...

--- ng/posts.ctrl.js
+++ ng/posts.ctrl.js
@@ -22,26 +22,16 @@ angular.module('app')
 	$scope.posts = posts;
   });
 
-  $scope.uploadFile = function() { // called from html upload button
-	   var file = $scope.myFile;
-	   console.log('file is ' );
-	   console.dir(file);
-	   //var uploadUrl = "/api/posts/fileUpload";
-	   PostsSvc.uploadFileToUrl(file); // call the upload function of the PostsSvc service
-  };
-})
-.directive('fileModel', ['$parse', function ($parse) { // directives in controller; ng-model does not work with input type "file"
-	return {
-	   restrict: 'A', // only matches attribute name
-	   link: function(scope, element, attrs) { // compiled only once, applicable to all instances of a directive (controller reactive)
-		  var model = $parse(attrs.fileModel); // $parse takes an expression and returns a function 
-		  var modelSetter = model.assign; // the object returned by $parse has an assign() method for setting values
-		  
-		  element.bind('change', function(){ // deprecated; like on()
-			 scope.$apply(function(){
-				modelSetter(scope, element[0].files[0]);
-			 });
-		  });
-	   }
+  $scope.uploadFile = function() { // from http://stackoverflow.com/questions/18571001/file-upload-using-angularjs
+	  var f = document.getElementById('ulfile').files[0],
+		  r = new FileReader();
+	  r.onloadend = function(e){
+	  var data = e.target.result;
+		//send your binary data via $http or $resource or do anything else with it
+		console.log('file is ' + data);
+		// todo: process data further
+	  }
+	  r.readAsBinaryString(f);
 	};
-}]);
+	// drag-and-drop upload: http://jsfiddle.net/vishalvasani/4hqVu/
+})
--- ng/posts.svc.js
+++ ng/posts.svc.js
@@ -9,18 +9,4 @@ angular.module('app')
   this.create = function (post) {
     return $http.post('/api/posts', post);
   }
-  
-  this.uploadFileToUrl = function(file) {
-	var fd = new FormData();
-	fd.append('file', file);
-	
-	// NEED MULTER --- body-parser does no longer support mulitpart
-	
-	// todo: keep it in temp storage till post has been saved then link, otherwise remove again
-	/*$http.post('/api/posts/fileUpload', fd, { // the file data are semt as sections in a multipart document in the body of the request
-		transformRequest: angular.identity, // undefined content-type and the transformRequest: angular.identity that give at the $http the ability to choose the right "content-type"
-		headers: {'Content-Type': undefined}
-	})*/
-	$http.post('/api/posts/fileUpload', fd);
-  }
 });
